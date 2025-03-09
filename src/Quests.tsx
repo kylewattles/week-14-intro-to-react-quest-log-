735fb41963fb4194c3f7d37580e258eaa9aa8e5b
kyle wattles <wattles.kyle66@Gmail.com>
2025-03-09 11:14:00 -0700

    added some css styling and finishing up

--- src/Quests.tsx
+++ src/Quests.tsx
@@ -6,7 +6,7 @@ export default function Quests({questName, deleteQuest }: { questName: string[],
 
     return (
     <div id="questlistbody" className="flex-grow-1 d-flex flex-column">
-        <div className="d-flex justify-content-center align-items-center bg-light">   
+        <div id="questlogbody" className="d-flex justify-content-center align-items-center bg-light">   
             <div className="bg-info m-3 w-75 shadow-lg p-3 border rounded">Quest Log</div>
         </div>
         <div id="questbody">
