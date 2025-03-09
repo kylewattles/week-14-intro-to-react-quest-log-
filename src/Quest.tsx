76b3027a8e22b00b83ee147e5ea92971f9df4830
kyle wattles <wattles.kyle66@Gmail.com>
2025-03-09 09:45:44 -0700

    created simple update button that turns green when clicked to signify active quest

--- src/Quest.tsx
+++ src/Quest.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
 export default function Quest({ quest, deleteQuest }: { quest: string, deleteQuest: () => void }) {
- 
+const [isActive, setIsActive] = useState(false);
+
+const handleActiveQuest = () => {
+  setIsActive(!isActive)
+}
   return (
     <div className="border-end bg-light p-3">
       <div>
@@ -7,6 +13,8 @@ export default function Quest({ quest, deleteQuest }: { quest: string, deleteQue
 
       </div>
       <button onClick={deleteQuest} className="btn btn-success shadow-lg">Finish Quest</button>
+      <button onClick={handleActiveQuest} className={`btn ${isActive ? "btn-success" : "btn-secondary"}
+      {isActive ? "Inactive" : "Active}`}>set as active quest</button>
     </div>
   );
 }
