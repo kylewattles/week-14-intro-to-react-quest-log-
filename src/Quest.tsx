c9396424519827dde03b7e643a4c8cf9219b1968
kyle wattles <wattles.kyle66@Gmail.com>
2025-03-15 19:21:37 -0700

    created animation for strikethrough on my quest button

--- src/Quest.tsx
+++ src/Quest.tsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 
 export default function Quest({ quest, deleteQuest }: { quest: string, deleteQuest: () => void }) {
 const [isActive, setIsActive] = useState(false);
+const [isStrikethrough, setIsStrikethrough] = useState(false)
 
 const handleActiveQuest = () => {
   setIsActive(!isActive)
 }
+
+const handleDeleteQuest = () => {
+  setIsStrikethrough(true)
+  setTimeout(deleteQuest, 700)
+}
   return (
     <div id="questbody" className="border-end bg-light p-3">
-      <div>
+      <div className={`quest-text ${isStrikethrough ? "strikethrough" : ""}`}>
       {quest}
-
       </div>
-      <button onClick={deleteQuest} className="btn btn-success shadow-lg">Finish Quest</button>
+      <div className="quest-buttons">
+      <button onClick={handleDeleteQuest} className="btn btn-success shadow-lg">Finish Quest</button>
       <button onClick={handleActiveQuest} className={`btn ${isActive ? "btn-warning" : "btn-secondary"}
       {isActive ? "Inactive" : "Active}`}>set as active quest</button>
+    </div>
     </div>
   );
 }
